246acf1ce8db160052b05a39058268e913ea31a9
Yani Ioannou <yani.ioannou@gmail.com>
2017-09-08 15:02:20 +0100

    Add wordcount script

---
+++ wordcount.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+texcount -v0 -inc dedication.tex declaration.tex acknowledgement.tex abstract.tex introduction.tex background.tex motivation.tex spatialbasis.tex filterbasis.tex conditional.tex conclusion.tex futurework.tex
