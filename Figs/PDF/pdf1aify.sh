73240fb9a232a987c9bb7d887ebf21d33f78a1ab
Yani Ioannou <yani.ioannou@gmail.com>
2017-09-17 23:28:23 +0100

    Add stuff to prevent gs changing images more

--- Figs/PDF/pdf1aify.sh
+++ Figs/PDF/pdf1aify.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Embed fonts and make sure version of pdfs are 1.4
 for file in *.pdf; do
-	gs -q -dNOPAUSE -dBATCH -dPDFSETTINGS=/prepress -dPDFA -dCompatibilityLevel=1.4 -sDEVICE=pdfwrite -sOutputFile=/tmp/${file} ${file} && mv /tmp/${file} .
+	gs -q -dColorConversionStrategy=/LeaveColorUnchanged, -dDownsampleMonoImages=false -dDownsampleGrayImages=false -dDownsampleColorImages=false -dAutoFilterColorImages=false -dAutoFilterGrayImages=false -dColorImageFilter=/FlateEncode -dGrayImageFilter=/FlateEncode -dNOPAUSE -dBATCH -dPDFSETTINGS=/prepress -dPDFA -dCompatibilityLevel=1.4 -sDEVICE=pdfwrite -sOutputFile=/tmp/${file} ${file} && mv /tmp/${file} .
 done
