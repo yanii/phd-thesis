08c7e94046e291c683421b467efe62a4f2c0d8ab
Yani Ioannou <yani.ioannou@gmail.com>
2017-09-30 19:54:32 +0100

    Add script for splitting PDF into chapters

---
+++ splitchapters.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#!/bin/bash
+
+infile=$1 # input pdf
+outputprefix=$2
+
+[ -e "$infile" -a -n "$outputprefix" ] || exit 1 # Invalid args
+
+pagenumbers=( $(pdftk "$infile" dump_data | \
+		grep -A1 '^BookmarkLevel: 1' | grep '^BookmarkPageNumber: ' | cut -f2 -d' ' | uniq)
+              end )
+
+chapternames=( $(pdftk "$infile" dump_data | \
+		grep -B1 '^BookmarkLevel: 1' | grep '^BookmarkTitle: ' | cut -f2,3 -d' ' | tr ' ' '-'| uniq)
+              end )
+for ((i=0; i < ${#pagenumbers[@]} - 1; ++i)); do
+  a=${pagenumbers[i]} # start page number
+  b=${pagenumbers[i+1]} # end page number
+  [ "$b" = "end" ] || b=$[b-1]
+  pdftk "$infile" cat $a-$b output "${outputprefix}"_${chapternames[i]}.pdf
+done
